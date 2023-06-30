f4415dcafc2b79b6f9a93499885b5c03cb0cab6c
mazin <mazink2@gmail.com>
2023-06-29 22:58:24 -0400

    Updated task service to use an observable

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  constructor() {}
 
-  constructor() { }
-
-  getTasks(): Task[] {
-    return TASKS;
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
   }
 }
